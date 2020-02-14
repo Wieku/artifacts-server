311a9f0613e6453f3750f0c5ec44e3546b0f1ba0
Sebastian Krajewski <mail@wieku.me>
2020-02-14 19:59:56 +0100

    Move "/api/v1/artifacts/{artifactId}" to "/api/v1/artifacts/by_id/{artifactId}"

--- src/main/kotlin/org/artifactscracow/artifactsserver/controllers/ArtifactController.kt
+++ src/main/kotlin/org/artifactscracow/artifactsserver/controllers/ArtifactController.kt
@@ -17,30 +17,31 @@ public class ArtifactController {
 
     @Autowired
     private lateinit var repository: ArtifactRepository
+
     @Autowired
     private lateinit var security: SecurityManager
 
-    @RequestMapping(value = ["/api/v1/artifacts/in_area"], method = [RequestMethod.GET])
+    @GetMapping(value = ["/api/v1/artifacts/in_area"])
     fun getArtifacts(@RequestParam(value = "lat1") lat1: Double, @RequestParam(value = "lon1") lon1: Double, @RequestParam(value = "lat2") lat2: Double, @RequestParam(value = "lon2") lon2: Double): ResponseEntity<Any> {
         val artifacts = repository.getInArea(lat1, lon1, lat2, lon2).stream().map { artifact -> ArtifactPoint(artifact.id, artifact.latitude, artifact.longitude) }.collect(Collectors.toList())
         return ResponseEntity.ok(artifacts)
     }
 
-    @RequestMapping(value = ["/api/v1/artifacts/by_id/{artifactId}"], method = [RequestMethod.GET])
+    @GetMapping(value = ["/api/v1/artifacts/by_id/{artifactId}"])
     fun getArtifact(@PathVariable artifactId: UUID): ResponseEntity<Any> {
         val artifact = repository.getArtifact(artifactId)
         return if (artifact == null) ResponseEntity.notFound().build() else ResponseEntity.ok(ArtifactView(artifact))
     }
 
-    @RequestMapping(value = ["/api/v1/artifacts"], method = [RequestMethod.POST])
+    @PostMapping(value = ["/api/v1/artifacts"])
     fun addArtifact(@RequestBody(required = true) artifactBody: ArtifactAdd, @RequestHeader(value = "Authorization", required = false) token: String?): ResponseEntity<Any> {
         repository.addArtifact(artifactBody, token != null && security.isAuthenticated(token))
         return ResponseEntity.noContent().build()
     }
 
-    @DeleteMapping(value = ["/api/v1/artifacts/{artifactId}"])
-    fun removeArtifact(@PathVariable artifactId: UUID, @RequestHeader(value = "Authorization", required = false) token: String?): ResponseEntity<Any> {
-        if (token == null || !security.isAuthenticated(token)) return ResponseEntity(HttpStatus.UNAUTHORIZED)
+    @DeleteMapping(value = ["/api/v1/artifacts/by_id/{artifactId}"])
+    fun removeArtifact(@PathVariable artifactId: UUID, @RequestHeader(value = "Authorization") token: String): ResponseEntity<Any> {
+        if (!security.isAuthenticated(token)) return ResponseEntity(HttpStatus.UNAUTHORIZED)
         return if (repository.removeArtifact(artifactId)) ResponseEntity.noContent().build() else ResponseEntity.notFound().build()
     }
 
