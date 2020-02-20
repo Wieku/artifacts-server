a0c3168d40af8ab9da88787f0f7dcdfc761abe22
Sebastian Krajewski <mail@wieku.me>
2020-02-20 22:18:39 +0100

    Use env vars for secrets (previous vars changed for security)

--- src/main/kotlin/org/artifactscracow/artifactsserver/security/Constants.kt
+++ src/main/kotlin/org/artifactscracow/artifactsserver/security/Constants.kt
@@ -1,4 +1,4 @@
 package org.artifactscracow.artifactsserver.security
 
-const val PBKDF2Secret = "GFEYFGIFUut47iwseyfhuhGYLNQFGRJHiuhIu4ggYYGRJYRGFURf7uguy"
-const val JWTSecret = "t8y7teiyfhetMZXe47cte74tie7hftjveiec7muevtehtye7teGOkYIHFIURH7436gfFDFBJyuh"
+val PBKDF2Secret: String = System.getenv("PBKDF2_SECRET")
+val JWTSecret: String = System.getenv("JWT_SECRET")
