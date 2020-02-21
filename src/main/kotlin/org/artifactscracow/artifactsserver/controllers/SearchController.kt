133e607977e6f2be4556df4fd8d27b571cf3f29c
Sebastian Krajewski <mail@wieku.me>
2020-02-21 03:25:40 +0100

    Add paging to search

--- src/main/kotlin/org/artifactscracow/artifactsserver/controllers/SearchController.kt
+++ src/main/kotlin/org/artifactscracow/artifactsserver/controllers/SearchController.kt
@@ -3,6 +3,7 @@ package org.artifactscracow.artifactsserver.controllers
 import org.artifactscracow.artifactsserver.repository.artifact.ArtifactRepository
 import org.artifactscracow.artifactsserver.views.ArtifactView
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.PageRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -15,11 +16,11 @@ class SearchController {
     private lateinit var repository: ArtifactRepository
 
     @GetMapping(value = ["/api/v1/artifacts/search"])
-    fun searchArtifacts(@RequestParam(value = "name", required = false) name: String?, @RequestParam(value = "type", required = false) type: String?, @RequestParam(value = "street", required = false) street: String?, @RequestParam(value = "building", required = false) building: String?): ResponseEntity<Any> {
+    fun searchArtifacts(@RequestParam(value = "name", required = false) name: String?, @RequestParam(value = "type", required = false) type: String?, @RequestParam(value = "street", required = false) street: String?, @RequestParam(value = "building", required = false) building: String?, @RequestParam(value = "page", required = false) page: Int?, @RequestParam(value = "size", required = false) size: Int?): ResponseEntity<Any> {
         if (name.isNullOrBlank() && type.isNullOrBlank() && street.isNullOrBlank() && building.isNullOrBlank())
             return ResponseEntity.badRequest().build()
-        val artifacts = repository.findAllByContent_NameContainsAndContent_TypeContainsAndContent_StreetContainsAndContent_BuildingContains(
-                name ?: "", type ?: "", street ?: "", building ?: "")
+        val artifacts = repository.findAllByContent_NameContainsAndContent_TypeContainsAndContent_StreetContainsAndContent_BuildingContainsOrderByContent_EditedAtDesc(
+                name ?: "", type ?: "", street ?: "", building ?: "", PageRequest.of(page ?: 0, size ?: 10))
         return ResponseEntity.ok(artifacts.map { ArtifactView(it) })
     }
 
--- src/main/kotlin/org/artifactscracow/artifactsserver/repository/artifact/ArtifactRepository.kt
+++ src/main/kotlin/org/artifactscracow/artifactsserver/repository/artifact/ArtifactRepository.kt
@@ -8,6 +8,6 @@ import java.util.*
 
 internal interface ArtifactRepository : PagingAndSortingRepository<Artifact, UUID>, ArtifactRepositoryCustom {
     fun findAllByContentNotNullOrderByCreatedAtDesc(pagination: Pageable): Page<Artifact>
-    fun findAllByContent_NameContainsAndContent_TypeContainsAndContent_StreetContainsAndContent_BuildingContains(name: String, type: String, street: String, building: String): List<Artifact>
+    fun findAllByContent_NameContainsAndContent_TypeContainsAndContent_StreetContainsAndContent_BuildingContainsOrderByContent_EditedAtDesc(name: String, type: String, street: String, building: String, pagination: Pageable): Page<Artifact>
     fun countAllByContentNotNull(): Int
 }
